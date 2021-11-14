bf15831b2de7d75246057a1006b73556d8c3f415
Dee Schaedler <dominicschaedler@gmail.com>
2021-11-14 16:53:52 -0500

    fix persistance issues

--- app/app.rb
+++ app/app.rb
@@ -4,9 +4,9 @@ class App
   def tick
     @z_layer = Array.new(3) { [] }
 
-    checkbox = UICheckbox.new({ x: 100, y: 100, w: 16, h: 16 })
-    checkbox.tick
-    @z_layer[0] << checkbox.draw
+    @checkbox ||= UICheckbox.new({ x: 100, y: 100, w: 16, h: 16 })
+    @checkbox.tick
+    @z_layer[0] << @checkbox.draw
 
     z_draw(layers: @z_layer)
   end

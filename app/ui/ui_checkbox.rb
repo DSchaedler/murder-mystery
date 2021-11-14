a69928625a1d53f02710c3bcf26360514e1495f9
Dee Schaedler <dominicschaedler@gmail.com>
2021-11-14 16:52:14 -0500

    fix rectangle formatting

--- app/ui/ui_checkbox.rb
+++ app/ui/ui_checkbox.rb
@@ -12,7 +12,7 @@ class UICheckbox
 
   def tick
     args = $gtk.args
-    if args.inputs.mouse.up && args.geometry.inside_rect? [args.inputs.mouse.x, args.inputs.mouse.y, 1, 1] [@x, @y, @w, @h]
+    if args.inputs.mouse.up && args.geometry.inside_rect?([args.inputs.mouse.x, args.inputs.mouse.y, 1, 1], [@x, @y, @w, @h])
       puts "checkbox clicked"
       case @state
       when :empty || :checked_false
